738ad1adb78c2f9a2cfe2476c5c6a8c3064f1fb2
Charles Rozsa <chuckeroz@gmail.com>
2015-10-22 10:20:00 -0600

    Checking for valid user.

--- Application.java
+++ Application.java
@@ -3,6 +3,9 @@ import java.sql.*;
 
 public class Application{
 
+  public String client_email = "";
+  public String client_password = "";
+
   private String m_userName;
   private String m_password;
   private String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
@@ -19,13 +22,13 @@ public class Application{
     System.out.println("1 - Registered User");
     System.out.println("2 - Not a Registered User");
     System.out.println("q - Exit");
-    String result = in.nextLine();
+    String result = in.nextLine()
+
     if (result.equals("1")){
-      System.out.print("Please enter your email: ");
-      String email = in.next();
-      System.out.print("Please enter your password: ");
-      String pass = in.next();
-      System.out.println(email + " " + pass);
+
+      while(!app.Login()){}
+      System.out.println("Valid Credentials!")
+
     }
     else if(result.equals("2")){
 
@@ -34,9 +37,55 @@ public class Application{
 
     }
 
-    Connection m_con;
-    String createString;
+  }
 
+  public Boolean Login(){
+    Boolean valid = FALSE;
+
+    System.out.print("Please enter your email: ");
+    app.client_email = in.next();
+    System.out.print("Please enter your password: ");
+    app.password = in.next();
+
+    Connection m_con;
+    String findUsers;
+    findUsers = "SELECT email, pass " +
+    "FROM users";
+    Statement stmt;
+
+    try
+    {
+      Class drvClass = Class.forName(m_driverName);
+      DriverManager.registerDriver((Driver)
+      drvClass.newInstance());
+    } catch(Exception e)
+    {
+      System.err.print("ClassNotFoundException: ");
+      System.err.println(e.getMessage());
+    }
+
+    try
+    {
+      m_con = DriverManager.getConnection(m_url, m_userName, m_password);
+
+      stmt = m_con.createStatement();
+      ResultSet rst = stmt.executeUpdate(createString);
+      while(rst.next()){
+        if (rst.getString(1).equals(app.client_email) && rst.getString(2).equals(app.client_password)){
+          valid = TRUE;
+          break;
+        }
+      }
+      rst.close();
+      stmt.close();
+      m_con.close();
+    } catch(SQLException ex) {
+      System.err.println("SQLException: " +
+      ex.getMessage());
+    }
+
+
+      return valid;
   }
 
 
