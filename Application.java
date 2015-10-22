f4925aa2500637b7f925bb50409123f81e78c5aa
Charles Rozsa <chuckeroz@gmail.com>
2015-10-22 09:49:50 -0600

    Testing Input

--- Application.java
+++ Application.java
@@ -11,10 +11,14 @@ public class Application{
   public static void main(String args[]) {
     Application app = new Application();
 
-
-
     app.m_userName = args[0];
     app.m_password = args[1];
+
+    System.out.println("Pick an option:")
+    System.out.println("1 - Registered User")
+    System.out.println("2 - Not a Registered User")
+    System.out.println("q - Exit")
+    String result = System.in.readnext();
 
     Connection m_con;
     String createString;
