f41a7edfd73921bca7b6c82cc43246d54814c2e6
ChuckRozsa <chuckeroz@gmail.com>
2015-10-21 21:43:11 -0600

    Added files for deleting data from assignment 2 and adding tables for project.

---
+++ deleteData.sql
@@ -0,0 +1,10 @@
+delete from flights_fares;
+delete from bookings;
+delete from tickets;
+delete from passengers;
+delete from flights;
+delete from sch_flights;
+delete from fares;
+delete from airports;
+
+commit;
