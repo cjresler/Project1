200e3c8cf23d476ba3f4a7f0f4b85846544a36a3
ChuckRozsa <chuckeroz@gmail.com>
2015-10-26 16:01:16 -0600

    Testing

--- Application.java
+++ Application.java
@@ -262,7 +262,7 @@ public class Application{
 			                "min(a1.price+a2.price), a1.dep_time, a2.arr_time " +
 			                "from available_flights a1, available_flights a2 " +
 			                "where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time " +
-			                "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time ";
+			                "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time, a1.dep_time, a2.arr_time ";
       threeFlights = "create view two_connections (src,dst,dep_date,flightno1,flightno2, flightno3, layover,price, dep_time, arr_time) as " +
 			                  "select a1.src, a3.dst, a1.dep_date, a1.flightno, a2.flightno, a3.flightno, (a2.dep_time-a1.arr_time+(a3.dep_time-a2.arr_time)), " +
 			                  "min(a1.price+a2.price+a2.price), a1.dep_time, a3.arr_time " +
