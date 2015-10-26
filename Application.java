0e75a59ac1c1ecde1dd115c7b67e755836638cc9
ChuckRozsa <chuckeroz@gmail.com>
2015-10-25 19:08:04 -0600

    Attempting to initView

--- Application.java
+++ Application.java
@@ -129,12 +129,60 @@ public class Application{
           ret_date = in.next();
           ret_dateparts = ret_date.split("-");
         }
-
+        app.initViews(app);
 
         app.Menu(app);
       }
 
 
+
+    }
+
+    public void initViews(Application app)
+    {
+
+      Connection m_con;
+      String dropViews,singleFlight;
+      dropViews = "DROP VIEW available_flights"
+      singleFlight = "create view available_flights(flightno,dep_date, src,dst,dep_time,arr_time,fare,seats,price) as" +
+                      "select f.flightno, sf.dep_date, f.src, f.dst, f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))," +
+                      "f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))+(f.est_dur/60+a2.tzone-a1.tzone)/24," +
+                      "fa.fare, fa.limit-count(tno), fa.price" +
+                      "from flights f, flight_fares fa, sch_flights sf, bookings b, airports a1, airports a2" +
+                      "where f.flightno=sf.flightno and f.flightno=fa.flightno and f.src=a1.acode and" +
+                      "f.dst=a2.acode and fa.flightno=b.flightno(+) and fa.fare=b.fare(+) and" +
+                      "sf.dep_date=b.dep_date(+)" +
+                      "group by f.flightno, sf.dep_date, f.src, f.dst, f.dep_time, f.est_dur,a2.tzone," +
+                      "a1.tzone, fa.fare, fa.limit, fa.price" +
+                      "having fa.limit-count(tno) > 0";
+      Statement stmt;
+
+      try
+      {
+        Class drvClass = Class.forName(m_driverName);
+        DriverManager.registerDriver((Driver)
+        drvClass.newInstance());
+      } catch(Exception e)
+      {
+        System.err.print("ClassNotFoundException: ");
+        System.err.println(e.getMessage());
+      }
+
+      try
+      {
+        m_con = DriverManager.getConnection(app.m_url, app.m_userName, app.m_password);
+
+        stmt = m_con.createStatement();
+        stmt.executeQuery(dropViews)
+        stmt.executeQuery(singleFlight);
+
+
+        rst.close();
+        stmt.close();
+        m_con.close();
+      } catch(SQLException ex) {
+
+      }
 
     }
 
