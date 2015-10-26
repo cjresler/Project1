c384daf7bac75f2474fb1ec6aed4d161eee73914
ChuckRozsa <chuckeroz@gmail.com>
2015-10-26 17:04:57 -0600

    Fixed

--- Application.java
+++ Application.java
@@ -150,7 +150,7 @@ public class Application{
           two_connections =
                       "union " +
                       "SELECT flightno as fno, '' fno2, '' fno3, to_char(dep_date, 'DD-MM-YYYY') as dep_date, src,dst,to_char(dep_time, 'HH24:MI') as dep, " +
-                      "to_char(arr_time, 'HH24:MI') as arr,fare,price, 2 stops " +
+                      "to_char(arr_time, 'HH24:MI') as arr,price, 2 stops " +
                       "FROM two_connections " +
                       "WHERE src = '" + src + "' and dst = '" + dst + "' " +
                       "AND to_char(dep_date, 'DD-MM-YYYY') = '" + dep_date + "' ";
