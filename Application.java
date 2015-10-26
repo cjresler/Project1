4e5fbc1f2fa88d77796d4b66eef875bfabedc908
ChuckRozsa <chuckeroz@gmail.com>
2015-10-26 16:02:36 -0600

    Should be fixed.

--- Application.java
+++ Application.java
@@ -268,7 +268,7 @@ public class Application{
 			                  "min(a1.price+a2.price+a2.price), a1.dep_time, a3.arr_time " +
 			                  "from available_flights a1, available_flights a2, available_flights a3 " +
 			                  "where a1.dst=a2.src and a2.dst=a3.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time and a2.arr_time +1.5/24 <=a3.dep_time and a2.arr_time +5/24 >=a3.dep_time " +
-			                  "group by a1.src, a3.dst, a1.dep_date, a1.flightno, a2.flightno, a3.flightno, a2.dep_time, a1.arr_time, a3.dep_time, a2.arr_time ";
+			                  "group by a1.src, a3.dst, a1.dep_date, a1.flightno, a2.flightno, a3.flightno, a2.dep_time, a1.arr_time, a3.dep_time, a2.arr_time, a1.dep_time, a3.arr_time ";
       Statement stmt;
 
 
