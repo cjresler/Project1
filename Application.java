1d6a8e9a53ac98f6611e4adda6af5eabc2fddcef
ChuckRozsa <chuckeroz@gmail.com>
2015-10-27 08:57:51 -0600

    Test

--- Application.java
+++ Application.java
@@ -340,7 +340,7 @@ public class Application{
         //Insert flights into bookings
         System.out.println("Fare3");
         String insertB1 = "insert into bookings values(" + ticket_number + ", '" + fno + "', '" +
-                            fare + "', " + rs.getObject(5) + ", 'A20')";
+                            fare + "', " + rs.getDate(5) + ", 'A20')";
 
           System.out.println("B1");
 
